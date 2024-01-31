ce7ec72f4723d6da250ddfd3edfedd38ec0035c9
xlooslo <109078547+xlooslo@users.noreply.github.com>
2024-01-31 15:14:30 +0900

    Update H-Index.py

--- 알고리즘 고득점 Kit/정렬/H-Index.py
+++ 알고리즘 고득점 Kit/정렬/H-Index.py
@@ -1,6 +1,6 @@
 # 처음에는 4 4 4 2 1 1일 때 배열에 있는 값 중에 H-Index가 결정된다고 생각했으나
-# H-Index는 꼭 배열에 있는 값이 아니어도 된다는 것을 알게 됐다.
-# 위의 예시에서 답은 3이다.
+# H-Index는 꼭 배열에 있는 값이 아니어도 된다는 것을 알게 됨
+# 위의 예시에서 답은 3임
 def solution(citations):
     citations.sort(reverse=True)
     n = len(citations)
